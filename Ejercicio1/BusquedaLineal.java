fe73826b92198ca13e58d7162f982dc7d9a941fa
JosueAtiencia <atiencia.jxsue10@gmail.com>
2026-05-19 21:07:49 -0500

    Ejercicio1 Mejorado

--- Ejercicio1/BusquedaLineal.java
+++ Ejercicio1/BusquedaLineal.java
@@ -9,8 +9,9 @@ public class BusquedaLineal {
         String[] productos = new String[5];
         double[] precios = new double[5];
 
+        int opcion;
         String buscar;
-        boolean encontrado = false;
+        boolean encontrado;
 
         // Ingreso de productos y precios
         for (int i = 0; i < 5; i++) {
@@ -23,28 +24,55 @@ public class BusquedaLineal {
             sc.nextLine();
         }
 
-        // Buscar producto
-        System.out.print("\nIngrese el producto a buscar: ");
-        buscar = sc.nextLine();
+        do {
+            //Creacion de menu de opciones
+            System.out.println("\n===== MENU =====");
+            System.out.println("1. Buscar producto");
+            System.out.println("2. Finalizar programa");
+            System.out.print("Seleccione una opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
 
-        // Búsqueda lineal
-        for (int i = 0; i < 5; i++) {
+            switch (opcion) {
 
-            if (productos[i].equalsIgnoreCase(buscar)) {
+                case 1:
 
-                System.out.println("\nProducto encontrado");
-                System.out.println("Nombre: " + productos[i]);
-                System.out.println("Precio: $" + precios[i]);
+                    encontrado = false;
 
-                encontrado = true;
-                break;
+                    System.out.print("\nIngrese el producto a buscar: ");
+                    buscar = sc.nextLine();
+
+                    // Búsqueda lineal
+                    for (int i = 0; i < 5; i++) {
+
+                        if (productos[i].equalsIgnoreCase(buscar)) {
+
+                            System.out.println("\nProducto encontrado");
+                            System.out.println("Nombre: " + productos[i]);
+                            System.out.println("Precio: $" + precios[i]);
+
+                            encontrado = true;
+                            break;
+                        }
+                    }
+
+                    if (!encontrado) {
+                        System.out.println("\nEl producto no existe.");
+                    }
+
+                    break;
+
+                case 2:
+
+                    System.out.println("\nPrograma finalizado.");
+                    break;
+
+                default:
+
+                    System.out.println("\nOpcion invalida.");
             }
-        }
 
-        // Producto no encontrado
-        if (!encontrado) {
-            System.out.println("\nEl producto no existe.");
-        }
+        } while (opcion != 2);
 
         sc.close();
     }
